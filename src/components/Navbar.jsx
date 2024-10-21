b7efcd853cfecd91d3bc3760cd0de05799f182f6
unknown <ayyasudeen@gmail.com>
2024-10-21 23:00:23 +0530

    nav items added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,13 @@ const Navbar = () => {
                         VirtualR
                     </span>
                 </div>
+                <ul className='hidden lg:flex ml-14 space-x-12'>
+                    {navItems.map((item, index) => (
+                        <li key={index}>
+                            <a href={item.href}>{item.label}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     </nav>
