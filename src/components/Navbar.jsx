66aae4bb706c6c6208e77fbbca15fda22b4f5f77
unknown <ayyasudeen@gmail.com>
2024-10-21 22:59:57 +0530

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import logo from "../assets/logo.png"
+import {navItems} from "../constants";
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-        
+        <div className="container px-4 mx-auto relative text-sm">
+            <div className="flex justify-center items-center">
+                <div className="flex items-center flex-shrink-0">
+                    <img src={logo} alt="logo" className="h-10 w-10 mr-2" />
+                    <span className="text-xl tracking-tight">
+                        VirtualR
+                    </span>
+                </div>
+            </div>
+        </div>
     </nav>
   )
 }
