aeb9bcfc0224a24f9d7c273fccb749cc9e7d28ee
unknown <ayyasudeen@gmail.com>
2024-11-06 18:51:44 +0530

    herosection

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <>
       <Navbar />
-      <HeroSection />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   );
 }
