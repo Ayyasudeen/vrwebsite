9734f9b6dd3cacaa3d583293e9009482833c5553
unknown <ayyasudeen@gmail.com>
2024-11-06 18:44:43 +0530

    Navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   );
 }
